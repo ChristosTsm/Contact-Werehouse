cb02de0c5628e63a7e7ea8e1aaa662bc3ca1de5d
TsmRayden <rayden.tsm@gmail.com>
2019-06-27 20:40:47 +0300

    Contact Add

--- routes/auth.js
+++ routes/auth.js
@@ -32,26 +32,21 @@ async (req,res) => {
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array() });
     }
-
     const{email,password} = req.body;
-
     try {
         let user = await User.findOne({email});
         if(!user) {
             return res.status(400).json({msg:"Invalid Credentials"})
         }
         const isMatch = await bcrypt.compare(password,user.password);
-
         if(!isMatch){
             return res.status(400).json({msg: 'Invalid Credentials'});
         }
-
         const payload = {
             user : {
                 id: user.id
             }
         }
-
         jwt.sign(payload,config.get('jwtSecret'), { 
             expiresIn:36000
         }, 
